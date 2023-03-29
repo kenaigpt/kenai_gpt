b7770b062ccca907266d207db189641c9b8d7ed1
kenaigpt <129308091+kenaigpt@users.noreply.github.com>
2023-03-29 23:41:23 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://kenai-gpt.onrender.com/', {
+    const response = await fetch('https://kenai-gpt.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
